1b81f8167a3a2e6565158870857b816f4bb4651e
trodrigues1505 <74118138+trodrigues1505@users.noreply.github.com>
2026-06-09 23:02:24 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, getDoc, deleteDoc,
+         onSnapshot, query, where }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyALLyOiOhZlrLSitXqyFmSPxYK5YzGIupk",
-  authDomain: "foco-projeto.firebaseapp.com",
-  projectId: "foco-projeto",
-  storageBucket: "foco-projeto.firebasestorage.app",
+  apiKey:            "AIzaSyALLyOiOhZlrLSitXqyFmSPxYK5YzGIupk",
+  authDomain:        "foco-projeto.firebaseapp.com",
+  projectId:         "foco-projeto",
+  storageBucket:     "foco-projeto.firebasestorage.app",
   messagingSenderId: "856677777681",
-  appId: "1:856677777681:web:503f820cefa8a422c20cb0",
-  measurementId: "G-MCFBZEJH8Z"
+  appId:             "1:856677777681:web:503f820cefa8a422c20cb0"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app      = initializeApp(firebaseConfig);
+const auth     = getAuth(app);
+const db       = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider, GoogleAuthProvider, signInWithPopup, signOut,
+         onAuthStateChanged, collection, doc, setDoc, getDoc, deleteDoc,
+         onSnapshot, query, where };
